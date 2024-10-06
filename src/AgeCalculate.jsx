53744e109f6b4bf05ab48d744e25a40af657434e
hard-to-smart <2403punjabidivya@gmail.com>
2024-10-07 01:47:39 +0530

    completed

--- src/AgeCalculate.jsx
+++ src/AgeCalculate.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Display from './Display'
 
-const AgeCalculate = () => {
+const AgeCalculate = ({age}) => {
   return (
-    <div>AgeCalculate</div>
+    <div className='overlay'>
+    <div className='card' style={{display:'absolute', border:'2px solid black' }}>
+      {
+        age >=18 ? (
+          <h2>You are eligible to vote</h2>
+        ) :
+        (
+          <h2>You are not eligible to vote</h2>
+        )
+      }
+    </div>
+    </div>
   )
 }
 
--- src/Display.jsx
+++ src/Display.jsx
@@ -1,49 +1,45 @@
 import React, { useEffect, useState } from 'react'
+import AgeCalculate from './AgeCalculate'
 
 const Display = () => {
-  const [showInfo, setShowInfo] = useState(false)
-  const [userDate, setUserDate] = useState({})
-  const [currentDate, setCurrentDate] = useState(new Date())
+
+  const [showInfo, setShowInfo] = useState(false) 
+  const [error, setError] = useState(false)
   const [age, setAge] = useState(null);
 
   const handleShowInfo=()=>{
     setShowInfo((prev)=> !prev);
   }
+
   const handleFetchUserAge=(e)=>{
     e.preventDefault()
     const formData = new FormData(e.target);
     const date = formData.get('date');
-    setUserDate(date);
+    handleGetAge(date);
   }
 
-useEffect(() => {
-    setCurrentDate(new Date());
-}, []);
-
-useEffect(()=>{
-
-  handleGetAge(userDate, currentDate);
-}, [userDate, currentDate])
-
-const handleGetAge = (dob, currentDate) => {
-    const birthDate = new Date(dob);
+const handleGetAge = (userdob) => {
+    if (!userdob) {
+      setError(true);
+      return;
+    }
+    const currentDate = new Date();
+    const birthDate = new Date(userdob);
     const ageDiff = currentDate - birthDate;
-    console.log(ageDiff)
     const ageInYears = new Date(ageDiff).getUTCFullYear() - 1970;
     setAge(ageInYears);
 };
 
-console.log(currentDate, userDate, age);
-
-
   return (
+    <>
     <div className='wrapper'>
-    <card className="card">
+    <div className="card">
       <h2>Find if you are eligible to use our service</h2>
         <form onSubmit={handleFetchUserAge}>
           <label htmlFor='date'> What is your date of birth ?</label>
 
-            <input id="date" name='date' type='date' />
+            <input id="date" name='date' type='date'/>
+            {error && <span className='error'> Date of Birth is required</span> }
             <button type='submit'>Check</button>
         </form>
         <h3 onClick={handleShowInfo}>Why do we need to check this?</h3>
@@ -52,15 +48,12 @@ console.log(currentDate, userDate, age);
             Your date of birth determines if you can use our platform. You must be at least 18 years old to use our services.
         </p>
         }
-        {age !== null ? (
-          age >= 18 ? (
-            <p>You are eligible to vote</p>
-          ) : (
-            <p>You are ineligible to vote</p>
-          )
-        ) : null}
-    </card>
     </div>
+    </div>
+     { age &&
+      <AgeCalculate age={age}/>
+     }
+     </>
   )
 }
 
